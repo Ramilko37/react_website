47b78ac964091fc5a82dfa78232bd4539d8d6525
Ramilko37 <galyamdin@gmail.com>
2021-03-26 19:16:30 +0300

    Updates

---
+++ precache-manifest.fb52a37c99b36a3fb4c59b2eacfb6d36.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62edf1a993417bf890a49d4c5bc382ef",
+    "url": "/react_website/index.html"
+  },
+  {
+    "revision": "654c20a2245f773bde4b",
+    "url": "/react_website/static/css/main.60992c15.chunk.css"
+  },
+  {
+    "revision": "0330614e1e6700c62639",
+    "url": "/react_website/static/js/2.bbe6a070.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react_website/static/js/2.bbe6a070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654c20a2245f773bde4b",
+    "url": "/react_website/static/js/main.c82df7a6.chunk.js"
+  },
+  {
+    "revision": "46c945ec6bd027fbb294",
+    "url": "/react_website/static/js/runtime-main.96012bd4.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/react_website/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/react_website/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/react_website/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/react_website/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "c64ec5c8d3ea8c3743ca51c1fa59e50e",
+    "url": "/react_website/static/media/img-5.c64ec5c8.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/react_website/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/react_website/static/media/img-7.66001cf8.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/react_website/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "523a89f64e53deb7aa02e9ff0fa6d18f",
+    "url": "/react_website/static/media/img-9.523a89f6.jpg"
+  },
+  {
+    "revision": "369d2a847d6d7aa6ef24fc4b1a17204f",
+    "url": "/react_website/static/media/img-home.369d2a84.jpg"
+  }
+]);
