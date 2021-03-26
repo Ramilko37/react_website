318ad22e4d9a664cd35fc31ad88be2d5818683f6
Ramilko37 <galyamdin@gmail.com>
2021-03-26 11:39:21 +0300

    Content section completed & images added

---
+++ src/components/Cards.js
@@ -0,0 +1,52 @@
+import React from "react";
+import './Cards.css';
+import CardItem from "./CardItem";
+import { images } from '../utils/images';
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <h1>Check out these EPIC Destinations </h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem
+                        src={images.img}
+                        text='Explore the hidden waterfall
+                         deep inside the Amazon Jungle'
+                        label='Adventure'
+                        path='/services'
+                        />
+                        <CardItem
+                            src={images.img_2}
+                            text='Travel through the islands of Bali in a Private Cruise'
+                            label='Luxury'
+                            path='/services'
+                        />
+                        <CardItem
+                            src={images.img_3}
+                            text='Explore the hidden waterfall
+                         deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src={images.img_2}
+                            text='Travel through the islands of Bali in a Private Cruise'
+                            label='Luxury'
+                            path='/services'
+                        />
+                        <CardItem
+                            src={images.img}
+                            text='Travel through the islands of Bali in a Private Cruise'
+                            label='Luxury'
+                            path='/services'
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards;
---
+++ src/utils/images.js
@@ -0,0 +1,26 @@
+import img from '../images/img-1.jpg';
+import img_2 from '../images/img-2.jpg';
+import img_3 from '../images/img-3.jpg';
+import img_4 from '../images/img-4.jpg';
+import img_5 from '../images/img-5.jpg';
+import img_6 from '../images/img-6.jpg';
+import img_7 from '../images/img-7.jpg';
+import img_8 from '../images/img-8.jpg';
+import img_9 from '../images/img-9.jpg';
+import img_home from '../images/img-home.jpg';
+
+const images =
+    {
+        img,
+        img_2,
+        img_3,
+        img_4,
+        img_5,
+        img_6,
+        img_7,
+        img_8,
+        img_9,
+        img_home
+    };
+
+export { images };
